ebb5bd360898f4c9da3b43ab82410ec99e4e986d
= <=>
2012-07-24 03:31:25 +0800

    Minimize sum of squares

---
+++ ps4.3.py
@@ -0,0 +1,10 @@
+#
+# Given a list of numbers, L, find a number, x, that
+# minimizes the sum of the square of the difference
+# between each element in L and x: SUM_{i=0}^{n-1} (L[i] - x)^2
+# 
+# Your code should run in Theta(n) time
+# 
+
+def minimize_square(L):
+    return (sum(L) + 0.0) / len(L)
